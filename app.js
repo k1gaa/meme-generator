9cf3bfcd5adfeae88f5c1de5dba194d55a586aaf
k1gaa <k1gaa@icloud.com>
2024-02-26 21:47:20 +0100

    added download function

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const canvas = document.createElement("canvas");
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const form = document.querySelector("form");
 const imageInput = document.querySelector("#controls__image");
 const titleInput = document.querySelector("#controls__title");
 
-const img = new Image();
+let img = new Image();
+let fileName = "";
 let title = "";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  title = titleInput.value;
+
   const fileReader = new FileReader();
 
   if (imageInput.files[0]) {
+    fileName = imageInput.files[0].name;
     fileReader.readAsDataURL(imageInput.files[0]);
   }
 
@@ -22,8 +26,6 @@ form.addEventListener("submit", (e) => {
     };
     img.src = fileReader.result;
   });
-
-  title = titleInput.value;
 });
 
 const createMeme = () => {
@@ -35,5 +37,10 @@ const createMeme = () => {
   ctx.fillStyle = "rgb(255, 255, 255)";
   ctx.fillText(title, 0, 50);
 
-  document.body.appendChild(canvas);
+  let a = document.createElement("a");
+  a.href = canvas.toDataURL("image/jpeg");
+  a.download = `${fileName.slice(0, -4)} meme`;
+  a.innerHTML = "download";
+
+  document.body.appendChild(a);
 };
